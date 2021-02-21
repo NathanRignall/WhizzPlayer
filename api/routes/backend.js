a741de933aa081419c8a1630f978818b2b8c7a14
NathanRignall <rignallnathan@gmail.com>
2021-02-21 21:48:43 +0000

    fix(all): Some basic fixes
    Fixed an error after removing "info" area of backend route in api
    Changed some minor formatting of back routes

--- api/routes/backend.js
+++ api/routes/backend.js
@@ -4,7 +4,6 @@ var router = express.Router();
 
 // functions
 var grab = require("../functions/backend/grab");
-var info = require("../functions/backend/info");
 
 // middleware
 var auth = require("../middleware/auth");
--- back/routes/test.js
+++ back/routes/test.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-
 var fs = require("fs");
 var stringify = require("csv-stringify");
 
