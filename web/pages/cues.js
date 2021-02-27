3a0f4913413f7d4f34a141c1ecf422ff4dc49a4c
NathanRignall <rignallnathan@gmail.com>
2021-02-27 00:08:38 +0000

    feat(web): first web commit
    added main layout
    added headers, footers and navbars
    imported bootsrap css
    added template cues and tracks pages
    updated index to have basic fetch
    cleared out css

---
+++ web/pages/cues.js
@@ -0,0 +1,5 @@
+import Layout from "../components/layouts/main";
+
+export default function Main() {
+    return <Layout title="Home">Cues</Layout>;
+}
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,5 +1,42 @@
-function About() {
-  return <div>About</div>;
-}
+import Layout from "../components/layouts/main";
 
-export default About;
+import Link from "next/link";
+import useSWR from "swr";
+
+const fetcher = async (url) => {
+    const res = await fetch(url);
+
+    // If the status code is not in the range 200-299,
+    // we still try to parse and throw it.
+    if (!res.ok) {
+        const error = new Error("An error occurred while fetching the data.");
+        // Attach extra info to the error object.
+        error.info = await res.json();
+        error.status = res.status;
+        throw error;
+    }
+
+    return res.json();
+};
+
+export default function Main() {
+    const { data, error } = useSWR(
+        process.env.NEXT_PUBLIC_API_URL + "/app/status",
+        fetcher
+    );
+
+    if (error) {
+        console.log(error);
+        return <>errror</>;
+    }
+
+    if (!data) {
+        return <>loading</>;
+    }
+    if (data) {
+        console.log(data);
+        return <>data</>;
+    } else {
+        return <>WTF</>;
+    }
+}
