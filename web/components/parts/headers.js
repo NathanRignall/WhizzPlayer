87c2180cac4aaea509887b310789510b6ab7075e
NathanRignall <rignallnathan@gmail.com>
2021-05-14 23:16:04 +0100

    feat(web icons): new favicons for site

--- web/components/parts/headers.js
+++ web/components/parts/headers.js
@@ -3,7 +3,37 @@ import Head from "next/head";
 export default function Header(props) {
     return (
         <Head>
-            <title>{props.title}</title>
+            <title>WhizzPlayer - {props.title}</title>
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/icons/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/icons/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/icons/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/icons/site.webmanifest" />
+            <link
+                rel="mask-icon"
+                href="/icons/safari-pinned-tab.svg"
+                color="#343a40"
+            />
+            <link rel="shortcut icon" href="/icons/favicon.ico" />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta
+                name="msapplication-config"
+                content="/icons/browserconfig.xml"
+            />
+            <meta name="theme-color" content="#ffffff"></meta>
         </Head>
     );
 }
